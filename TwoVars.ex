1af5d24163c41f9c7bd2dbe226382b660892643a
kammitama5 <krystal.maughan@gmail.com>
2016-07-27 12:30:07 -0700

    fourth commit

---
+++ TwoVars.ex
@@ -0,0 +1,9 @@
+#write a module that takes two values (c and d) and produces (2c + 3d).
+#call it TwoVars and input= input1
+
+defmodule TwoVars do
+    def var(input, input1) do
+        ((2*input) + (3*input1))
+    end
+end
+
